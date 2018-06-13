f737cacd19119cba47dfef512d827de939dbad74
acornard <alexis.cornard@openio.io>
2018-06-13 18:45:32 +0200

    Fix formatting issues

--- main/gridinit_cmd.c
+++ main/gridinit_cmd.c
@@ -399,36 +399,36 @@ command_status(int lvl, int argc, char **args)
 
 	/* write the title */
 	switch (lvl) {
-		case 0:
-			g_snprintf(fmt_title, sizeof(fmt_title),
-					"%%-%us %%-8s %%6s %%s\n",
-					(guint)maxkey);
-			g_snprintf(fmt_line, sizeof(fmt_line),
-					"%%-%us %%s %%6d %%s\n",
-					(guint)maxkey);
-			fprintf(stdout, fmt_title, "KEY", "STATUS", "PID", "GROUP");
-			break;
-		case 1:
-			g_snprintf(fmt_title, sizeof(fmt_title),
-					"%%-%us %%-8s %%5s %%6s %%5s %%19s %%%us %%s\n",
-					(guint)maxkey, (guint)maxgroup);
-			g_snprintf(fmt_line, sizeof(fmt_line),
-					"%%-%us %%s %%5d %%6d %%5d %%19s %%%us %%s\n",
-					(guint)maxkey, (guint)maxgroup);
-			fprintf(stdout, fmt_title, "KEY", "STATUS", "PID", "#START",
-					"#DIED", "SINCE", "GROUP", "CMD");
-			break;
-		default:
-			g_snprintf(fmt_title, sizeof(fmt_title),
-					"%%-%us %%-8s %%5s %%6s %%5s %%8s %%8s %%8s %%19s %%%us %%s\n",
-					(guint)maxkey, (guint)maxgroup);
-			g_snprintf(fmt_line, sizeof(fmt_line),
-					"%%-%us %%s %%5d %%6d %%5d %%8ld %%8ld %%8ld %%19s %%%us %%s\n",
-					(guint)maxkey, (guint)maxgroup);
+	case 0:
+		g_snprintf(fmt_title, sizeof(fmt_title),
+				"%%-%us %%-8s %%6s %%s\n",
+				(guint)maxkey);
+		g_snprintf(fmt_line, sizeof(fmt_line),
+				"%%-%us %%s %%6d %%s\n",
+				(guint)maxkey);
+		fprintf(stdout, fmt_title, "KEY", "STATUS", "PID", "GROUP");
+		break;
+	case 1:
+		g_snprintf(fmt_title, sizeof(fmt_title),
+				"%%-%us %%-8s %%5s %%6s %%5s %%19s %%%us %%s\n",
+				(guint)maxkey, (guint)maxgroup);
+		g_snprintf(fmt_line, sizeof(fmt_line),
+				"%%-%us %%s %%5d %%6d %%5d %%19s %%%us %%s\n",
+				(guint)maxkey, (guint)maxgroup);
+		fprintf(stdout, fmt_title, "KEY", "STATUS", "PID", "#START",
+				"#DIED", "SINCE", "GROUP", "CMD");
+		break;
+	default:
+		g_snprintf(fmt_title, sizeof(fmt_title),
+				"%%-%us %%-8s %%5s %%6s %%5s %%8s %%8s %%8s %%19s %%%us %%s\n",
+				(guint)maxkey, (guint)maxgroup);
+		g_snprintf(fmt_line, sizeof(fmt_line),
+				"%%-%us %%s %%5d %%6d %%5d %%8ld %%8ld %%8ld %%19s %%%us %%s\n",
+				(guint)maxkey, (guint)maxgroup);
 
-			fprintf(stdout, fmt_title, "KEY", "STATUS", "PID", "#START",
-					"#DIED", "CSZ", "SSZ", "MFD", "SINCE", "GROUP", "CMD");
-			break;
+		fprintf(stdout, fmt_title, "KEY", "STATUS", "PID", "#START",
+				"#DIED", "CSZ", "SSZ", "MFD", "SINCE", "GROUP", "CMD");
+		break;
 	}
 
  print_lines:;
