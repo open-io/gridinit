95c2c3647f0870a4fedb8d594ff699bfd63a90a9
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2018-06-13 23:25:26 +0200

    Use glib-2.0 features when possible

--- main/utils.c
+++ main/utils.c
@@ -21,32 +21,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "./gridinit_internals.h"
 
-#define STR_SKIP_SPACES(s) do {\
-	register gchar c;\
-	for (; (c = *s) && g_ascii_isspace(c) ;++s);\
-} while (0)
-
-#define STR_TRIM_TRAILING_SPACES(s) do { \
-	register gchar c, *end; \
-	for (end = s; *end ;++end); \
-	-- end; \
-	for (; end > s && (c = *end) && g_ascii_isspace(c) ;--end) *end = '\0'; \
-} while (0)
-
 gboolean
 gridinit_group_in_set(const gchar *group, const gchar *set)
 {
-	gchar **tokens, **token, *g;
-
-	tokens = g_strsplit_set(set, ",", -1);
+	gchar **tokens = g_strsplit_set(set, ",", -1);
 	if (!tokens)
 		return 0;
-	for (token=tokens; (g = *token) ;token++) {
-		STR_SKIP_SPACES(g);
-		STR_TRIM_TRAILING_SPACES(g);
+	for (gchar **ptoken=tokens; *ptoken ;ptoken++) {
+		gchar *g = *ptoken;
 		if (!*g)
 			continue;
-		if (0 == g_ascii_strcasecmp(g, group)) {
+		if (0 == g_ascii_strcasecmp(g_strstrip(g), group)) {
 			g_strfreev(tokens);
 			return TRUE;
 		}
