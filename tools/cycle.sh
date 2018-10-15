732258ad62c6e17e219857970b27da7c2780ab5b
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2018-10-15 11:26:43 +0200

    ci: Test the JSON output

---
+++ tools/cycle.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+CMD="gridinit_cmd -S /tmp/gridinit/gridinit.sock"
+
+$CMD -f json status | jq
+
+for G in @test-0 @test-1 '' '@test-0 @test-1' ; do
+	$CMD status $G
+	$CMD stop $G
+	$CMD start $G
+	$CMD status $G
+done
+
+$CMD -f json status | jq
+
